d3ea8351b417eec030e2c4f29638697266c0cf51
sanaOS21 <sasasa21nn@gmail.com>
2023-01-15 13:36:52 +0900

    usersテーブルとsubjectsテーブルのリレーションを定義(提出）

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -17,6 +17,6 @@ class Subjects extends Model
 
     public function users()
     {
-        return $this->belongsToMany('App\Models\Users'); // リレーションの定義
+        return $this->belongsToMany('App\Models\Users\User', 'subject_users', 'subject_id', 'user_id'); // リレーションの定義
     }
 }
