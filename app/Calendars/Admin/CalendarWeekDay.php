a798919ca418f65615f299407fd51871f4dd6f05
sanaOS21 <sasasa21nn@gmail.com>
2023-02-15 17:05:45 +0900

    スクール予約確認画面の各部数にリンクを設置する

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -43,16 +43,16 @@ class CalendarWeekDay
       // スクールのカウント
       // $partCount =
       $html[] =
-        '<p class="day_part m-0 pt-1">1部<span
+        '<p class="day_part m-0 pt-1"><a href="' . route('calendar.admin.detail', ['id' => $one_part->id, 'data' => $one_part->setting_reserve, 'part' => $one_part->setting_part]) . '">1部</a><span
         style="margin-left: 15px">' . $one_part->users->count() . '</span></p>';
     }
     if ($two_part) {
       $html[] =
-        '<p class="day_part m-0 pt-1">2部<span style="margin-left:15px">' . $two_part->users->count() . '</span></p>';
+        '<p class="day_part m-0 pt-1"><a href="' . route('calendar.admin.detail', ['id' => $two_part->id, 'data' => $two_part->setting_reserve, 'part' => $two_part->setting_part]) . '">2部</a><span style="margin-left:15px">' . $two_part->users->count() . '</span></p>';
     }
     if ($three_part) {
       $html[] =
-        '<p class="day_part m-0 pt-1">3部<span style="margin-left:15px">' . $three_part->users->count() . '</span></p>';
+        '<p class="day_part m-0 pt-1"><a href="' . route('calendar.admin.detail', ['id' => $three_part->id, 'data' => $three_part->setting_reserve, 'part' => $three_part->setting_part]) . '">3部</a><span style="margin-left:15px">' . $three_part->users->count() . '</span></p>';
     }
     $html[] = '</div>';
 
